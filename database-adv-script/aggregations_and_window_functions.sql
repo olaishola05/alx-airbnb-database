d0b0b6dce4099d1e412270825ae616906d39bd1b
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-02 11:42:58 +0100

    Queries for aggregations & window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,21 @@
+SELECT user_id, COUNT(user_id) AS total_number_bookings 
+FROM bookings bks
+GROUP BY user_id
+
+WITH PropertyBookings AS(
+SELECT
+  p.property_id,
+  p.name,
+  COUNT(bks.property_id) AS total_number_bookings
+FROM properties AS p
+INNER JOIN
+bookings AS bks ON p.property_id = bks.property_id
+GROUP BY p.property_id, p.name
+)
+
+SELECT
+  property_id,
+  name,
+  total_number_bookings,
+  ROW_NUMBER() OVER (ORDER BY total_number_bookings DESC) as RankByBookings
+FROM PropertyBookings
